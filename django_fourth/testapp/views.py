9ed7e2498f534e5368a44808234fec0261e81d81
Sererim <danielleymanw@gmail.com>
2023-10-11 19:01:38 +0300

    Having troubles with image saving.

--- django_fourth/testapp/views.py
+++ django_fourth/testapp/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 from django.shortcuts import render
 from django.utils.timezone import datetime, timedelta
-from .models import Client, Order
+from .models import Client, Product, Order
+from PIL import Image
 
 
 def order_list(request):
@@ -32,3 +33,31 @@ def order_list(request):
             return render(request, 'testapp/order_list.html', context)
 
     return render(request, 'testapp/index.html')
+
+
+# View for upload_image.htnl
+def upload_image(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    
+    if request.method == 'POST' and request.FILES.get('image'):
+        image_file = request.FILES['image']
+        try:
+            img = Image.open(image_file)
+            product.image.save(image_file.name, img)
+            
+            return redirect('product_details', product=product_id)
+        except IOError:
+            product.image = None
+            product.save()
+            msg = "Failed to process the uploaded image."
+            return render(request, 'testapp/upload_image.html', {'product_id':product_id, 'error_message':msg})
+        
+    context = {'product_id':product_id}
+    return render(request, 'testapp/upload_image.html', context)
+
+
+# View for showign product.
+def product_details(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    context = {'product': product}
+    return render(request, 'testapp/product_details.html', context)
